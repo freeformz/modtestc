f0c6b35795e5d3888014767a3d2f79a0dc30cc32
Edward Muller <freeformz@gmail.com>
2019-11-14 12:42:34 -0800

    test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/freeformnz/modtestc
+
+go 1.13
+
+require github.com/freeformz/modtestb v0.0.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/freeformz/modtestb"
+)
+
+func main() {
+	fmt.Println(modtestb.Hello)
+}
